79a939f0821c666b8fabb8fdfd7888069d69c555
Pedro Henrique Nascimento Vieira <pedrohnv@hotmail.com>
2019-11-10 08:03:26 -0300

    Ignorar checkpoints na build

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 FILES="$(find notebooks -type f -name '*.ipynb')"
 for f in $FILES
 do
-    nb2hugo $f --site-dir base --section post
+    if [ ${file: -17} != "-checkpoint.ipynb" ]
+    then
+        nb2hugo $f --site-dir base --section post
+    fi
 done
 hugo -s base
